f87b992b9c2d853d64c770de4e3573877860c8f1
Alex <hueskyecla@hotmail.com>
2015-06-22 20:43:41 +0200

    aaa
    aaa

--- js/createLegend.js
+++ js/createLegend.js
@@ -1,7 +1,8 @@
 // JavaScript Document
-function create_legend(layer)
+function create_legend(layer,wmsurl)
 {
 	var name = layer.get('name');
-	 var codigohtml ="<img class=\"legendIcon\" src='http://localhost:8080/geoserver/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER="+name+"' />";
-	return codigohtml
+	 var htmlLegend = "<img class=\"legendIcon\""+ 
+					  "src='"+wmsurl+"?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER="+name+"' />";
+	return htmlLegend;
 }
